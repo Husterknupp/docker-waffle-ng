a08243aed8b82616fa252a3b9bcc1cec64410db2
husterknupp <husterknupp@outlook.com>
2017-04-02 02:04:31 +0200

    bootstrap: typescript2+Angular4+webpack2

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # docker-waffle-ng
+
+### dev mode (hot reload)
+npm start
+
+### prod mode (dist folder)
+npm run build
+
---
+++ config/webpack.dev.js
@@ -0,0 +1,26 @@
+var webpackMerge = require('webpack-merge');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var commonConfig = require('./webpack.common.js');
+var helpers = require('./helpers');
+
+// dist / file outputs are stored only virtually - not on disk
+// https://angular.io/docs/ts/latest/guide/webpack.html#!#configure-webpack
+module.exports = webpackMerge(commonConfig, {
+  devtool: 'cheap-module-eval-source-map',
+
+  output: {
+    path: helpers.root('dist'),
+    publicPath: 'http://localhost:8080/',
+    filename: '[name].js',
+    chunkFilename: '[id].chunk.js'
+  },
+
+  plugins: [
+    new ExtractTextPlugin('[name].css')
+  ],
+
+  devServer: {
+    historyApiFallback: true,
+    stats: 'minimal'
+  }
+});
