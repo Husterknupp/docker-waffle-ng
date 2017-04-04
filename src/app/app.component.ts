72f7230fedf6d9b929f59a86782f9eaf9d09339e
husterknupp <husterknupp@outlook.com>
2017-04-04 23:46:24 +0200

    add bootstrap and test components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
 import '../assets/css/styles.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent { 
+  values = '';
+
+  onKey(event: any) {
+    this.values += event.target.value + ' | ';
+  }
+
+}
