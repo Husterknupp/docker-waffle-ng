0a430cb419408a3bb3b068a03c7021d122a5c593
husterknupp <husterknupp@outlook.com>
2017-04-05 18:04:11 +0200

    simple list with hover and click event
    use ngFor, ngIf directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,49 @@ import { Component } from '@angular/core';
 import '../assets/css/styles.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
+
+export class Hero {
+  id: number;
+  name: string;
+  hover: boolean;
+}
+
+const HEROES: Hero[] = [
+  { id: 11, hover: false, name: 'Mr. Nice' },
+  { id: 12, hover: false, name: 'Narco' },
+  { id: 13, hover: false, name: 'Bombasto' },
+  { id: 14, hover: false, name: 'Celeritas' },
+  { id: 15, hover: false, name: 'Magneta' },
+  { id: 16, hover: false, name: 'RubberMan' },
+  { id: 17, hover: false, name: 'Dynama' },
+  { id: 18, hover: false, name: 'Dr IQ' },
+  { id: 19, hover: false, name: 'Magma' },
+  { id: 20, hover: false, name: 'Tornado' }
+];
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent { 
-  values = '';
+  heroes = HEROES;
+  selectedHero: Hero = this.heroes[0];
 
   onKey(event: any) {
-    this.values += event.target.value + ' | ';
+    console.log(event.target.value);
   }
+
+  onHoverOverHero(hero: Hero) {
+  	hero.hover = true;
+  }
+  
+  onHoverHeroOut(hero: Hero) {
+  	hero.hover = false;
+  }
+
+	onSelect(hero: Hero): void {
+	  this.selectedHero = hero;
+	}
 
 }
